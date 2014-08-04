031a5f1555afb474b51b963fe07cc5d271f0ce03
Ilya Osadchiy <osadchiy.ilya@gmail.com>
2014-08-05 02:16:13 +0300

    Changing run scripts of cifar

--- examples/cifar10/train_simple_pure_sb.sh
+++ examples/cifar10/train_simple_pure_sb.sh
@@ -3,4 +3,4 @@
 TOOLS=../../build/tools
 
 GLOG_logtostderr=1 $TOOLS/caffe.bin train \
-  --solver_proto_file=cifar10_quick_shareboost_solver.prototxt
+  --solver_proto_file=cifar10_quick_pure_shareboost_solver.prototxt
